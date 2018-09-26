65355d28dd6d5f2e421a54041fd068b0879c5deb
Mark Webb-Johnson <mark@webb-johnson.net>
2018-09-26 08:27:41 +0800

    Just warn about CRC errors, don't panic abort

--- components/esp32/system_api.c
+++ components/esp32/system_api.c
@@ -138,7 +138,8 @@ esp_err_t esp_efuse_mac_get_default(uint8_t* mac)
             }
         } else {
             ESP_LOGE(TAG, "Base MAC address from BLK0 of EFUSE CRC error, efuse_crc = 0x%02x; calc_crc = 0x%02x", efuse_crc, calc_crc);
-            abort();
+            /* MWJ: Just warn about CRC errors, don't panic abort */
+            /* abort(); */
         }
     }
     return ESP_OK;
